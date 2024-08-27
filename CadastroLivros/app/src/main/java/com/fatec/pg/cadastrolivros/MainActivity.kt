7f42780def7d0df7a8491b7a9c435cc3fc19a95d
Pedro Wolski <pedrobwolski@gmail.com>
2024-08-26 23:51:12 -0300

    CadastroLivros

---
+++ CadastroLivros/app/src/main/java/com/fatec/pg/cadastrolivros/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.fatec.pg.cadastrolivros
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fatec.pg.cadastrolivros.view.SecondActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val edtTitulo = findViewById<EditText>(R.id.edtTitulo)
+        val edtAutor = findViewById<EditText>(R.id.edtAutor)
+        val btnCadastro = findViewById<Button>(R.id.btnCadastrar)
+
+        btnCadastro.setOnClickListener {
+            val titulo = edtTitulo.text.toString()
+            val autor = edtAutor.text.toString()
+
+            val intent = Intent(this, SecondActivity::class.java)
+
+
+            val bundle = Bundle()
+
+            bundle.putString("Titulo", titulo)
+            bundle.putString("Autor", autor)
+
+            intent.putExtras(bundle)
+
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ CadastroLivros/app/src/main/java/com/fatec/pg/cadastrolivros/view/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.fatec.pg.cadastrolivros.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fatec.pg.cadastrolivros.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+
+
+    }
+}
