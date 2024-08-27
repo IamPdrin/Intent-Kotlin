10014572ebb72eb88db8f07485bf405367410be1
Pedro Wolski <pedrobwolski@gmail.com>
2024-08-27 01:36:01 -0300

    CadastroLivros

--- CadastroLivros/app/src/main/java/com/fatec/pg/cadastrolivros/MainActivity.kt
+++ CadastroLivros/app/src/main/java/com/fatec/pg/cadastrolivros/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,20 +22,23 @@ class MainActivity : AppCompatActivity() {
         val btnCadastro = findViewById<Button>(R.id.btnCadastrar)
 
         btnCadastro.setOnClickListener {
-            val titulo = edtTitulo.text.toString()
-            val autor = edtAutor.text.toString()
+            if (edtTitulo.text.toString().isEmpty() || edtAutor.text.toString().isEmpty()) {
+                Toast.makeText(this, "Preencha os valores corretamente!", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val titulo = edtTitulo.text.toString()
+                val autor = edtAutor.text.toString()
 
-            val intent = Intent(this, SecondActivity::class.java)
+                val intent = Intent(this, SecondActivity::class.java)
 
+                val bundle = Bundle()
+                bundle.putString("Titulo", titulo)
+                bundle.putString("Autor", autor)
+                intent.putExtras(bundle)
 
-            val bundle = Bundle()
+                startActivity(intent)
 
-            bundle.putString("Titulo", titulo)
-            bundle.putString("Autor", autor)
-
-            intent.putExtras(bundle)
-
-            startActivity(intent)
+            }
         }
 
     }
--- CadastroLivros/app/src/main/java/com/fatec/pg/cadastrolivros/view/SecondActivity.kt
+++ CadastroLivros/app/src/main/java/com/fatec/pg/cadastrolivros/view/SecondActivity.kt
@@ -1,11 +1,20 @@
 package com.fatec.pg.cadastrolivros.view
 
+import android.content.Intent
+import android.health.connect.datatypes.units.Length
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.fatec.pg.cadastrolivros.MainActivity
 import com.fatec.pg.cadastrolivros.R
+import org.w3c.dom.Text
+import java.lang.System.exit
+import kotlin.system.exitProcess
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +22,34 @@ class SecondActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
 
+        val bundle = intent.extras
+        val titulo = bundle?.getString("Titulo")
+        val autor = bundle?.getString("Autor")
 
+        val textTitulo = findViewById<TextView>(R.id.txtTitulo)
+        val textAutor = findViewById<TextView>(R.id.txtAutor)
+
+        val btnSair = findViewById<Button>(R.id.btnSair)
+        val btnVoltar = findViewById<Button>(R.id.btnVoltar)
+
+        if (titulo.toString().isEmpty() || autor.toString().isEmpty()) {
+            Toast.makeText(this, "Os valores precisam ser preenchidos corretamente!", Toast.LENGTH_SHORT).show()
+        } else{
+            textTitulo.text = titulo
+            textAutor.text = autor
+        }
+
+        btnSair.setOnClickListener {
+            finishAffinity()
+            exitProcess(0)
+        }
+
+        btnVoltar.setOnClickListener{
+            finish()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
